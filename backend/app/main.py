94c7712059e93bd6c5cc1c70e3a0748a8cf56144
Vane2005 <vanessaduranmona@gmail.com>
2025-11-01 14:23:57 -0500

    Enviando prueba Azure

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,8 @@ from presentation.routers.paciente_router import router as paciente_router
 from presentation.routers.terapia_router import router as terapia_router
 from config import jwt_config  # Asegura que la configuración JWT se cargue
 
+#PruebaAzure
+
 app = FastAPI()
 
 # CORS SETTINGS - Asegúrate de que esto esté antes de include_router
